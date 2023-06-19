aee06cd07bdf39cfa29a6af1d7694988eae5bd4e
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-06-19 21:36:47 +0100

    fixed homapage

--- src/screen/Homepage.jsx
+++ src/screen/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Menu from '../components/Menu'
 import Slider from '../components/Slider'
+import ComingSoon from '../components/ComingSoon'
 
 const Homepage = () => {
   return (
     <div>
       <Menu />
       <Slider />
+      <ComingSoon />
     </div>
   )
 }
